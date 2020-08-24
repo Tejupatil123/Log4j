6ccf03b57ebac57e6dbb11e86051b10d14ac41e6
Teju123 <tejaswinipatild1996@gmail.com>
2020-08-24 16:25:05 +0530

    Log4j

---
+++ src/com/java/Demo.java
@@ -0,0 +1,17 @@
+package com.java;
+
+import org.apache.log4j.Logger;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+		Logger log =Logger.getLogger(Demo.class);
+		
+		
+		
+		
+		
+		log.info("hello log4j");
+		log.fatal ("Welcome log4");
+	}
+}
