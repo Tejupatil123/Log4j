f9c5f749eb5509616a530b1e737cde13493eda83
Dnyaneshwar <chaudharid@gmail.com>
2020-08-24 16:43:03 +0530

    multiplication

--- src/com/java/Demo.java
+++ src/com/java/Demo.java
@@ -1,17 +1,15 @@
 package com.java;
 
-import org.apache.log4j.Logger;
+
 
 public class Demo {
 	public static void main(String[] args) {
-		
-		Logger log =Logger.getLogger(Demo.class);
-		
+	
 		
 		
-		
-		
-		log.info("hello log4j");
-		log.fatal ("Welcome log4");
+		int a=10;
+		int b=20;
+		int c=a*b;
+		System.out.println(c);
 	}
 }
